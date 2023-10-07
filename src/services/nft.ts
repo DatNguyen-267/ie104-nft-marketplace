7240b9513c20e61c0e27c76a7afdcc45aa5264f9
DatNguyen-267 <19520040@gm.uit.edu.vn>
2023-10-07 12:33:21 +0700

    [MINT-NFT]

--- src/services/nft.ts
+++ src/services/nft.ts
@@ -1,142 +1,117 @@
-import { ethers } from "ethers";
-import { NFTStorage } from "nft.storage";
-import { NFT_ABI } from "../abis";
-import { AppError, MARKETPLACE_ADDRESS, NFT_ADDRESS } from "../constants";
-import { ProviderOptions } from "../types";
-import { MetadataInput } from "../types/metadata";
-import { getProvider } from "./provider";
+import { ethers } from 'ethers'
+import { NFTStorage } from 'nft.storage'
+import { NFT_ABI } from '../abis'
+import { AppError, MARKETPLACE_ADDRESS, NFT_ADDRESS, STORAGE_API_KEY } from '../constants'
+import { ProviderOptions } from '../types'
+import { MetadataInput } from '../types/metadata'
+import { getDefaultProvider, getProvider } from './provider'
 
 // export const STORAGE_API_KEY = process.env.STORAGE_API_KEY || "";
-export const STORAGE_API_KEY = "";
-export async function createMetadata(
-  file: File,
-  title: string,
-  description: string
-) {
+export async function createMetadata(file: File, title: string, description: string) {
   try {
     if (!STORAGE_API_KEY) {
-      throw new Error(AppError.API_KEY_INVALID);
+      throw new Error(AppError.API_KEY_INVALID)
     }
-    const client = new NFTStorage({ token: STORAGE_API_KEY as string });
-    const img = new Blob([file]);
+    const client = new NFTStorage({ token: STORAGE_API_KEY as string })
+    const img = new Blob([file])
     const data: MetadataInput = {
       image: img,
       name: title,
       description,
-    };
-    const cid = await client.store(data);
-    return cid.url;
+    }
+    const cid = await client.store(data)
+    return cid
   } catch (error) {
-    throw error;
+    throw error
   }
 }
 async function approveSpenderToAccessNft(
   cltAddress: string,
   spenderAddress = MARKETPLACE_ADDRESS,
   tokenId: string,
-  options?: ProviderOptions
+  options?: ProviderOptions,
 ) {
   try {
-    const provider = getProvider(options?.provider);
-    const tokenContract = new ethers.Contract(
-      cltAddress,
-      NFT_ABI,
-      provider.getSigner()
-    );
-    const transaction: any = await tokenContract.approve(
-      spenderAddress,
-      tokenId
-    );
-    const transactionReceipt: any = await transaction.wait();
-    console.log("approve receipt:", transactionReceipt);
+    const provider = getProvider(options?.provider)
+    const tokenContract = new ethers.Contract(cltAddress, NFT_ABI, provider.getSigner())
+    const transaction: any = await tokenContract.approve(spenderAddress, tokenId)
+    const transactionReceipt: any = await transaction.wait()
+    console.log('approve receipt:', transactionReceipt)
   } catch (error) {
-    throw error;
+    throw error
   }
 }
-async function mintNFT(
+export async function mintNFT(
   cltAddress: string,
   addressTo: string,
   tokenUri: string,
-  options?: ProviderOptions
+  options?: ProviderOptions,
 ) {
   try {
-    const provider = getProvider(options?.provider);
-    const nftContract = new ethers.Contract(cltAddress, NFT_ABI, provider);
-    const transaction = await nftContract.safeMint(addressTo, tokenUri);
-    const transactionReceipt: any = await transaction.wait();
-    console.log("Mint receipt:", transactionReceipt);
-    return transactionReceipt;
+    const provider = getDefaultProvider()
+    const nftContract = new ethers.Contract(cltAddress, NFT_ABI, provider?.getSigner())
+
+    const transaction = await nftContract.safeMint(addressTo, tokenUri)
+    const transactionReceipt: any = await transaction.wait()
+    console.log('Mint receipt:', transactionReceipt)
+    return transactionReceipt
   } catch (error) {
-    throw error;
+    throw error
   }
 }
-export async function getTokenUri(
-  cltAddress: string,
-  tokenId: number,
-  options?: ProviderOptions
-) {
+export async function getTokenUri(cltAddress: string, tokenId: number, options?: ProviderOptions) {
   try {
-    const provider = getProvider(options?.provider);
-    const contract = new ethers.Contract(cltAddress, NFT_ABI, provider);
-    const tokenUri: string = await contract.tokenURI(tokenId);
-    return tokenUri;
+    const provider = getProvider(options?.provider)
+    const contract = new ethers.Contract(cltAddress, NFT_ABI, provider)
+    const tokenUri: string = await contract.tokenURI(tokenId)
+    return tokenUri
   } catch (error) {
-    throw error;
+    throw error
   }
 }
 
-async function getOwner(
-  cltAddress: string,
-  tokenId: number,
-  options?: ProviderOptions
-) {
+async function getOwner(cltAddress: string, tokenId: number, options?: ProviderOptions) {
   try {
-    const provider = getProvider(options?.provider);
-    const contract = new ethers.Contract(cltAddress, NFT_ABI, provider);
-    const addressOwner = contract.ownerOf(tokenId);
-    return addressOwner;
+    const provider = getProvider(options?.provider)
+    const contract = new ethers.Contract(cltAddress, NFT_ABI, provider)
+    const addressOwner = contract.ownerOf(tokenId)
+    return addressOwner
   } catch (error) {
-    throw error;
+    throw error
   }
 }
 
 async function getYourTokens(
   cltAddress: string,
   creatorAddress: string,
-  options?: ProviderOptions
+  options?: ProviderOptions,
 ) {
   try {
-    let listTokenId: number[] = [];
-    const provider = getProvider(options?.provider);
-    const contract = new ethers.Contract(cltAddress, NFT_ABI, provider);
-    let tokenId = 0;
+    let listTokenId: number[] = []
+    const provider = getProvider(options?.provider)
+    const contract = new ethers.Contract(cltAddress, NFT_ABI, provider)
+    let tokenId = 0
     while (true) {
       try {
-        const token = await contract.ownerOf(tokenId);
-        if (token.toLowerCase() === creatorAddress.toLowerCase())
-          listTokenId.push(tokenId);
-        tokenId++;
+        const token = await contract.ownerOf(tokenId)
+        if (token.toLowerCase() === creatorAddress.toLowerCase()) listTokenId.push(tokenId)
+        tokenId++
       } catch (error) {
-        break;
+        break
       }
     }
-    return listTokenId;
+    return listTokenId
   } catch (error) {
-    throw error;
+    throw error
   }
 }
 
-export function listenForTransactionMined(
-  transactionResponse: any,
-  provider: any
-) {
-  console.log(`Mining ${transactionResponse.hash}...`);
+export function listenForTransactionMined(transactionResponse: any, provider: any) {
+  console.log(`Mining ${transactionResponse.hash}...`)
   return new Promise((resolve, reject) => {
     provider.once(transactionResponse.hash, (transactionReceipt: any) => {
-      console.log(
-        `Completed with ${transactionReceipt.confirmations} confirmations`
-      );
-    });
-    Promise.resolve();
-  });
+      console.log(`Completed with ${transactionReceipt.confirmations} confirmations`)
+    })
+    Promise.resolve()
+  })
 }
