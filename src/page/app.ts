fd391727e6ff0302e2de1a41a41dc41232f6944c
NguyenHoangNam-19520171 <71280485+NguyenHoangNam-19520171@users.noreply.github.com>
2023-10-11 14:06:44 +0700

    update-home-page

--- src/page/app.ts
+++ src/page/app.ts
@@ -4,6 +4,7 @@ import './../styles/grid.css';
 import './../components/avatar/styles.css';
 import './../components/header/styles.css';
 import './../components/alert/styles.css';
+
 console.log("LP script");
 
 // ============================= Toggle PopUP ====================================
@@ -46,6 +47,36 @@ alertCancel.onclick = toggleAlertSigout;
 alertClose.onclick = toggleAlertSigout;
 signOut.onclick = toggleAlertSigout;
 
+// ============================= Active sroll card ====================================
+const cards = document.querySelectorAll('.cards');
+
+const setClasses = () => {
+    const classes = ['left', 'active', 'right'];
+    cards.forEach((card, index) => card.classList.add(classes[index]))
+}
 
 
+const changePositions = (e: any) => {
+    const clickedCard = e.currentTarget;
+    const activeCard = document.querySelector('.cards.active') as HTMLElement;
+    if (clickedCard.classList.contains('active')) return;
+    const classesFrom = e.currentTarget.className;
+    const classesTo = activeCard.className;
+    clickedCard.className = classesTo;
+    activeCard.className = classesFrom;
+}
+
+cards.forEach((card) => {
+    card
+        .addEventListener('click', changePositions)
+})
+
+setClasses();
+
+// =========================== Set image for card ======================================
+var nftEg = document.querySelectorAll<HTMLElement>('.nft-eg__content');
+console.log(nftEg)
+nftEg[0].style.backgroundImage = "url('https://uithcm-my.sharepoint.com/personal/19520171_ms_uit_edu_vn/Documents/IE104/image/NftEg2.png')";
+nftEg[1].style.backgroundImage = "url('https://uithcm-my.sharepoint.com/personal/19520171_ms_uit_edu_vn/Documents/IE104/image/NftEg.png')";
+nftEg[2].style.backgroundImage = "url('https://uithcm-my.sharepoint.com/personal/19520171_ms_uit_edu_vn/Documents/IE104/image/NftEg3.png')";
 
