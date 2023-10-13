aba3b442cab80a9cd10a41326b7ba357ae433342
TLBTrung-222 <baotrung2852003@gmail.com>
2023-10-13 22:49:21 +0700

    allow user to send Ether to NFT marketplace

--- src/page/connectWallet/main.ts
+++ src/page/connectWallet/main.ts
@@ -1,3 +1,4 @@
+import { ethers } from 'ethers'
 import { AppError, NFT_ADDRESS } from '../../constants'
 import { getDefaultProvider, getProvider } from '../../services'
 import { connect, switchToNetwork } from '../../services/connect'
@@ -7,7 +8,9 @@ import './styles.css'
 let showAccount = document.querySelector('.showAccount') as HTMLElement
 let walletAddress: string
 
+const btnSendEth = document.querySelector('#btn-sendEth') as HTMLButtonElement
 const btnMetamask = document.querySelector('#btn-metamask') as HTMLButtonElement
+
 const handleConnectWallet = async () => {
   await connect()
     .then((res) => {
@@ -29,5 +32,27 @@ const handleConnectWallet = async () => {
   showAccount.innerHTML = walletAddress
 }
 
+// Send 0.01 Ether to NFT marketplace
+const handleSendEth = async () => {
+  window.ethereum
+    .request({
+      method: 'eth_sendTransaction',
+      params: [
+        {
+          from: walletAddress,
+          to: NFT_ADDRESS,
+          value: ethers.utils.parseEther('0.001').toString(),
+          // Below information customizable by the user during MetaMask confirmation.
+          gasLimit: '0x5028',
+          maxPriorityFeePerGas: '0x3b9aca00',
+          maxFeePerGas: '0x2540be400',
+        },
+      ],
+    })
+    .then((txHash: any) => console.log(txHash))
+    .catch((error: any) => console.error(error))
+}
+
 const onClickInstallMetaMask = () => {}
 btnMetamask.onclick = handleConnectWallet
+btnSendEth.onclick = handleSendEth
