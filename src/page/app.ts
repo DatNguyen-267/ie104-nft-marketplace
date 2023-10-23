f8f81192c5040bc4c71eecba73c4f298b88ee7a0
NguyenHoangNam-19520171 <71280485+NguyenHoangNam-19520171@users.noreply.github.com>
2023-10-23 18:04:12 +0700

    update-list-nft

--- src/page/app.ts
+++ src/page/app.ts
@@ -5,9 +5,12 @@ import './../components/avatar/styles.css';
 import './../components/header/styles.css';
 import './../components/alert/styles.css';
 import './../components/button/styles.css';
+import './../components/NFTcard/styles.css';
+import './../components/modal/modalBuyNFT/styles.css';
 
 console.log("LP script");
 
+// ========================== Header =======================================
 const btnLogin = document.getElementById("btn-login") as HTMLButtonElement;
 const popUpUserClose = document.getElementById("close-pop-up-user") as HTMLElement;
 const headerAvatar = document.getElementById("header-avatar") as HTMLElement;
@@ -16,9 +19,8 @@ const alertCancel = document.getElementById("alert-cancel") as HTMLElement;
 const alertClose = document.getElementById("alert-close") as HTMLElement;
 const signOut = document.getElementById("header-sign-out") as HTMLElement;
 
-// ========================== Check Login =======================================
+// Check Login 
 let login:boolean = false ;
-
 if (login === true as boolean) {
     headerAvatar.style.visibility = 'flex';
     btnLogin.style.display = 'none';
@@ -28,7 +30,7 @@ else {
     btnLogin.style.display = 'flex';
 }
 
-// ============================= Toggle PopUP ====================================
+// Toggle PopUP 
 function togglePopUpUser(event: Event): void {
     event.preventDefault();
     var x = document.getElementById("pop-up-user") as HTMLElement;
@@ -40,11 +42,10 @@ function togglePopUpUser(event: Event): void {
         x.style.opacity = "0";
     }
 }
-
 popUpUserClose.onclick = togglePopUpUser;
 headerAvatar.onclick = togglePopUpUser;
 
-// ============================== Toggle Alert===================================
+// Toggle Alert
 const toggleAlertSigout = (event: any) => {
     event.preventDefault();
     var x = document.getElementById("alert-sigout") as HTMLElement;
@@ -54,20 +55,19 @@ const toggleAlertSigout = (event: any) => {
         x.style.visibility = "hidden";
     }
 }
-
 alertOverlay.onclick = toggleAlertSigout;
 alertCancel.onclick = toggleAlertSigout;
 alertClose.onclick = toggleAlertSigout;
 signOut.onclick = toggleAlertSigout;
 
-// ============================= Active sroll card ====================================
-const cards = document.querySelectorAll('.cards');
+// ============================= INFORMATION ====================================
 
+// Active sroll card 
+const cards = document.querySelectorAll('.cards');
 const setClasses = () => {
     const classes = ['left', 'active', 'right'];
     cards.forEach((card, index) => card.classList.add(classes[index]))
 }
-
 const changePositions = (e: any) => {
     const clickedCard = e.currentTarget;
     const activeCard = document.querySelector('.cards.active') as HTMLElement;
@@ -77,18 +77,45 @@ const changePositions = (e: any) => {
     clickedCard.className = classesTo;
     activeCard.className = classesFrom;
 }
-
 cards.forEach((card) => {
     card
         .addEventListener('click', changePositions)
 })
-
 setClasses();
 
-// =========================== Set image for card ======================================
+// Set image for card 
 var nftEg = document.querySelectorAll<HTMLElement>('.nft-eg__content');
 console.log(nftEg)
 nftEg[0].style.backgroundImage = "url('https://uithcm-my.sharepoint.com/personal/19520171_ms_uit_edu_vn/Documents/IE104/image/NftEg2.png')";
 nftEg[1].style.backgroundImage = "url('https://uithcm-my.sharepoint.com/personal/19520171_ms_uit_edu_vn/Documents/IE104/image/NftEg.png')";
 nftEg[2].style.backgroundImage = "url('https://uithcm-my.sharepoint.com/personal/19520171_ms_uit_edu_vn/Documents/IE104/image/NftEg3.png')";
+
+// ===================== NFTs ============================
+const modalOverlay = document.getElementById("modal-buy-overlay-close") as HTMLElement;
+const modalCancel = document.getElementById("modal-buy-cancel") as HTMLElement;
+const modalClose = document.getElementById("modal-buy-close") as HTMLElement;
+const nfts = document.querySelectorAll<HTMLElement>(".nft-item");
+
+// Toggle modal nft
+const toggleModalBuyNFT = (event: any) => {
+    event.preventDefault();
+    var x = document.getElementById("modal-buy") as HTMLElement;
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+modalOverlay.onclick = toggleModalBuyNFT;
+modalCancel.onclick = toggleModalBuyNFT;
+modalClose.onclick = toggleModalBuyNFT;
+
+// load nfts
+document.addEventListener("DOMContentLoaded", () => {
+    for (let i = 0; i < nfts.length; i++) {
+        const btn = nfts[i].querySelector('.nft-card__btn') as HTMLButtonElement;
+        btn.onclick = toggleModalBuyNFT;
+    }
+})
 
