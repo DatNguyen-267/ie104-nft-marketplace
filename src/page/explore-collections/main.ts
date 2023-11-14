81e5a3007a113fc9fdab9344df04c7d99dc05467
NguyenHoangNam-19520171 <71280485+NguyenHoangNam-19520171@users.noreply.github.com>
2023-11-14 19:38:17 +0700

    create-ui-page-collections

--- src/page/explore-collections/main.ts
+++ src/page/explore-collections/main.ts
@@ -1,3 +1,12 @@
 import { ExploreCollectionPageControllerInstance } from './controller'
+import './styles.css'
+import './../../components/alert/styles.css'
+import './../../components/avatar/styles.css'
+import './../../components/button/styles.css'
+import './../../components/header/styles.css'
+import './../../components/loading/loading2/styles.css'
+import './../../components/collection/styles.css'
+import './../../styles/base.css'
+import './../../styles/grid.css'
 
 ExploreCollectionPageControllerInstance.getAllCollectionOfMarket()
