ff76ea6f02a89d30b64daa79fddb97179bf9192a
DatNguyen-267 <19520040@gm.uit.edu.vn>
2023-11-26 11:52:57 +0700

    small tweak

--- src/__test/modal.ts
+++ src/__test/modal.ts
@@ -1,51 +1,51 @@
-export enum LoadingId {
-  Container = 'container-loading',
-  LoadingText = 'loading-text',
-}
+// export enum LoadingId {
+//   Container = 'container-loading',
+//   LoadingText = 'loading-text',
+// }
 
-class LoadingController {
-  loadingText: string | null
-  constructor(_loadingText?: string) {
-    this.loadingText = _loadingText || null
-    this.listener()
-  }
+// class LoadingController {
+//   loadingText: string | null
+//   constructor(_loadingText?: string) {
+//     this.loadingText = _loadingText || null
+//     this.listener()
+//   }
 
-  set(_loadingText: string) {
-    this.loadingText = _loadingText
-    this.updateDomContent()
-  }
+//   set(_loadingText: string) {
+//     this.loadingText = _loadingText
+//     this.updateDomContent()
+//   }
 
-  get() {
-    return this.loadingText
-  }
+//   get() {
+//     return this.loadingText
+//   }
 
-  updateDomContent() {
-    const LoadingText = document.getElementById(LoadingId.LoadingText) as HTMLElement
+//   updateDomContent() {
+//     const LoadingText = document.getElementById(LoadingId.LoadingText) as HTMLElement
 
-    LoadingText.innerHTML = this.loadingText ? this.loadingText + '...' : 'LOADING...'
-  }
+//     LoadingText.innerHTML = this.loadingText ? this.loadingText + '...' : 'LOADING...'
+//   }
 
-  listener() {}
+//   listener() {}
 
-  close() {
-    const modal = document.getElementById(LoadingId.Container) as HTMLElement
-    modal.style.display = 'none'
-  }
+//   close() {
+//     const modal = document.getElementById(LoadingId.Container) as HTMLElement
+//     modal.style.display = 'none'
+//   }
 
-  toggle(event: any) {
-    event.preventDefault()
-    let modal = document.getElementById(LoadingId.Container) as HTMLElement
-    console.log(modal)
-    console.log(modal.style.display === 'none')
-    if (modal.style.display === 'none') {
-      modal.style.display = 'flex'
-    } else {
-      modal.style.display = 'none'
-    }
-  }
+//   toggle(event: any) {
+//     event.preventDefault()
+//     let modal = document.getElementById(LoadingId.Container) as HTMLElement
+//     console.log(modal)
+//     console.log(modal.style.display === 'none')
+//     if (modal.style.display === 'none') {
+//       modal.style.display = 'flex'
+//     } else {
+//       modal.style.display = 'none'
+//     }
+//   }
 
-  open() {
-    let modal = document.getElementById(LoadingId.Container) as HTMLElement
-    modal.style.display = 'flex'
-  }
-}
+//   open() {
+//     let modal = document.getElementById(LoadingId.Container) as HTMLElement
+//     modal.style.display = 'flex'
+//   }
+// }
